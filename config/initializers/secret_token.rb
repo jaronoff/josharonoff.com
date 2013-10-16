97122740194df9a1b37dc62db14227daa76415d5
Josh Aronoff <aronoff@gmail.com>
2013-10-16 13:47:22 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JosharonoffCom::Application.config.secret_token = '4ea51a0874403f6fb26437c273d15a55c185bec46fb316fbbb6abb883a2f332af8621d4e6fdf011288486ed6c2c3f1fead9ffb7f511420354a1c84a22155c14a'
